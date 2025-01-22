ab886ce284067f9ad6474110da32028e54a2fde2
Jason Thomas Brown <jason.thomas.brown@telenor.no>
2025-01-22 14:13:04 +0100

    more test and benchmark

--- gwt.edu/v2/array-slice/sum_test.go
+++ gwt.edu/v2/array-slice/sum_test.go
@@ -1,6 +1,9 @@
 package array_slice
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("add up a small array", func(t *testing.T) {
@@ -25,5 +28,18 @@ func assertArraySum(t testing.TB, expected, sum int, arr []int) {
 	if sum != expected {
 		t.Errorf("wanted '%d' got '%d'; array %v", expected, sum, arr)
 
+	}
+}
+
+func ExampleSum() {
+	arr := []int{2, 3, 4}
+	fmt.Print(Sum(arr))
+	// Output: 9
+}
+
+func BenchmarkSum(b *testing.B) {
+	arr := []int{4, 5, 6, 7, 8, 9, 12, 15, 18}
+	for range b.N {
+		Sum(arr)
 	}
 }
