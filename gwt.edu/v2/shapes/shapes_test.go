69dcf350e25a85bb08ffb4c6d9cd273bfda2933e
Jason Thomas Brown <jason.thomas.brown@telenor.no>
2025-01-22 15:53:35 +0100

    compiling but failing precision in test

--- gwt.edu/v2/shapes/shapes_test.go
+++ gwt.edu/v2/shapes/shapes_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestPerimter(t *testing.T) {
 	t.Run("square", func(t *testing.T) {
 		r := Rectangle{9.0, 9.0}
-		result := Perimeter(r)
+		result := r.Perimeter()
 		expected := 36.0
 		if result != expected {
 			t.Errorf("expected %f got %f", expected, result)
@@ -13,18 +13,19 @@ func TestPerimter(t *testing.T) {
 	})
 	t.Run("rect", func(t *testing.T) {
 		r := Rectangle{4.5, 1.12}
-		result := Perimeter(r)
+		result := r.Perimeter()
 		expected := 11.24
 		if result != expected {
 			t.Errorf("expected %f got %f", expected, result)
 		}
 	})
+
 }
 
 func TestArea(t *testing.T) {
 	t.Run("square", func(t *testing.T) {
 		r := Rectangle{9.0, 9.0}
-		result := Area(r)
+		result := r.Area()
 		expected := 81.0
 		if result != expected {
 			t.Errorf("expected %f got %f", expected, result)
@@ -32,10 +33,19 @@ func TestArea(t *testing.T) {
 	})
 	t.Run("rect", func(t *testing.T) {
 		r := Rectangle{2.0, 1.25}
-		result := Area(r)
+		result := r.Area()
 		expected := 2.5
 		if result != expected {
 			t.Errorf("expected %f got %f", expected, result)
 		}
+	})
+	t.Run("circle", func(t *testing.T) {
+		c := Circle{1.25}
+		expected := 4.9087
+		result := c.Area()
+		if result != expected {
+			t.Errorf("got '%g' want '%f", result, expected)
+		}
+
 	})
 }
