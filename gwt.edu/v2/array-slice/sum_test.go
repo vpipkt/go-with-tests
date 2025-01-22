63a101bd82e42b6a9d24da7b8a6d22213b46d954
Jason Thomas Brown <jason.thomas.brown@telenor.no>
2025-01-22 13:28:27 +0100

    working test

---
+++ gwt.edu/v2/array-slice/sum_test.go
@@ -0,0 +1,22 @@
+package array_slice
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	t.Run("add up a small array", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		expected := 15
+		got := Sum(numbers)
+
+		assertArraySum(t, expected, got, numbers)
+
+	})
+}
+
+func assertArraySum(t testing.TB, expected, sum int, arr []int) {
+	t.Helper()
+	if sum != expected {
+		t.Errorf("wanted '%d' got '%d'; array %v", expected, sum, arr)
+
+	}
+}
