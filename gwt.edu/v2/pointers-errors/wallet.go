180001eedc3c85a09b3dc10ee22adfe182397886
Jason Thomas Brown <jason.thomas.brown@telenor.no>
2025-01-23 10:16:08 +0100

    vanity type

--- gwt.edu/v2/pointers-errors/wallet.go
+++ gwt.edu/v2/pointers-errors/wallet.go
@@ -1,13 +1,15 @@
 package wallet
 
+type Mycoin int
+
 type Wallet struct {
-	balance int
+	balance Mycoin
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Mycoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Mycoin {
 	return w.balance
 }
--- gwt.edu/v2/pointers-errors/wallet_test.go
+++ gwt.edu/v2/pointers-errors/wallet_test.go
@@ -7,11 +7,11 @@ import (
 func TestWallet(t *testing.T) {
 
 	wallet := Wallet{}
-	wallet.Deposit(10)
+	wallet.Deposit(Mycoin(10))
 
 	got := wallet.Balance()
 
-	want := 10
+	want := Mycoin(10)
 
 	if got != want {
 		t.Errorf("got '%d' want '%d'", got, want)
